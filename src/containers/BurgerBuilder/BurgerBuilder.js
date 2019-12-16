c8fee0148b76179cf243bb2b8e6cfbafd1dc86cf
jackdo69 <jackie.duc.do@gmail.com>
2019-12-16 15:44:44 +1100

    disable button when item is 0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,7 +37,7 @@ class BurgerBuilder extends Component {
     removeIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         if (oldCount <= 0) {
-            return
+            return;
         }
         const updatedCount = oldCount - 1;
         const updatedIngredients = {
@@ -51,12 +51,19 @@ class BurgerBuilder extends Component {
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        };
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients} />
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler} />
+                    ingredientRemoved={this.removeIngredientHandler} 
+                    disabled={disabledInfo} />
             </Aux>
         );
     }
