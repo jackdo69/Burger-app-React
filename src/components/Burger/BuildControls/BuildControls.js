ad4018dcf50e4a89f01159c17adab99f2563f8d6
jackdo69 <jackie.duc.do@gmail.com>
2019-12-16 22:01:30 +1100

    cancle, continue button, add backdrop

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -14,6 +14,7 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
+        <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
              <BuildControl
                key={ctrl.label}
@@ -22,6 +23,10 @@ const buildControls = (props) => (
                removed={() => props.ingredientRemoved(ctrl.type)} 
                disabled={props.disabled[ctrl.type]}/>
         ))}
+        <button 
+        onClick={props.ordered}
+        disabled={!props.purchaseable}
+        className={classes.OrderButton}>ORDER NOW</button>
     </div>
 );
 
---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
+
+const orderSummary = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+        return (
+        <li key={igKey}><span style={{textTransform: 'capitalize'}}> {igKey} </span>
+         : {props.ingredients[igKey]} </li> );
+    });
+    return (
+        <Aux>
+            <h3>You order</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to checkout?</p>
+            <Button
+                clicked={props.purchasedCancelled}
+                btnType="Danger">CANCEL</Button>
+            <Button
+                clicked={props.purchasedContinued}
+                btnType="Success">CONTINUE</Button>
+        </Aux>
+    );
+};
+
+export default orderSummary;
